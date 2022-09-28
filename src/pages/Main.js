863631e19c03c1e4d012289e2e9c54fa673ba08f
enticies <ismayilaj@code.edu.az>
2022-09-28 10:54:58 +0400

    Finished delete request.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,188 +6,241 @@ import '../assets/styles/main.css';
 import jwt from 'jwt-decode';
 
 export default function Main() {
-    const [currentUser, setCurrentUser] = useState("");
+    const [currentUser, setCurrentUser] = useState('');
     const [notes, setNotes] = useState([]);
-    const [currentNote, setCurrentNote] = useState((notes && notes[0]) || {});
-    const [title, setTitle] = useState(
-        (currentNote && currentNote.noteTitle) || ""
-    );
-    const [body, setBody] = useState(
-        (currentNote && currentNote.noteBody) || ""
-    );
-    const [ accessToken, setAccessToken ]= useState(null);
-
+    const [currentNote, setCurrentNote] = useState(null);
+    const [title, setTitle] = useState((currentNote && currentNote.noteTitle) || '');
+    const [body, setBody] = useState((currentNote && currentNote.noteBody) || '');
     const navigate = useNavigate();
+    const [accessToken, setAccessToken] = useState('');
+
+    useEffect(() => {
+        setAccessToken(localStorage.getItem('accessToken'));
+        console.log('ffff');
+    }, []);
 
     const debouncedSave = React.useCallback(
-        debounce((notes) => {
-        }, 200),
+        debounce((note, accessToken) => {
+            if (!note || !accessToken) {
+                return;
+            }
+            console.log('ddd');
+            const body = {
+                id: note._id,
+                title: note.title,
+                body: note.body,
+            };
+            fetch(process.env.REACT_APP_API_URL + '/updatenote', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + accessToken,
+                },
+                credentials: 'include',
+                body: JSON.stringify(body),
+            })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }, 500),
         []
     );
 
     useEffect(() => {
-        setAccessToken(localStorage.getItem('accessToken'));
         try {
             setCurrentUser(jwt(accessToken).UserInfo.username);
+        } catch (err) {
+            // request new token with a refresh token here
         }
-        catch(err) {
-            // request new token with a refresh token here 
-        }
+    }, [accessToken]);
 
-           fetch(process.env.REACT_APP_API_URL + '/getusernotes', {
-            method: 'GET',
+    useEffect(() => {
+        if (accessToken) {
+            fetch(process.env.REACT_APP_API_URL + '/getusernotes', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + accessToken,
+                },
+                credentials: 'include',
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    setNotes(data);
+                    if (notes) {
+                        setCurrentNote(data[0]);
+                    }
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    }, [accessToken]);
+
+    const createNewNote = (e) => {
+        fetch(process.env.REACT_APP_API_URL + '/createnote', {
+            method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + accessToken 
+                Authorization: 'Bearer ' + accessToken,
             },
-            credentials: "include",
+            credentials: 'include',
         })
             .then((response) => {
+                console.log(response);
                 return response.json();
             })
             .then((data) => {
-                console.log(data);
+                setCurrentNote(data);
             })
             .catch((error) => {
-                console.error("Error:", error);
+                console.error('Error:', error);
             });
-    }, [])
-
-    function createNewNote(e) {
-        const newNote = {
-            noteTitle: "",
-            noteBody: "",
-            noteID: nanoid(),
-        };
-        setCurrentNote(newNote);
-        setTitle("");
-        setBody("");
+        setTitle('');
+        setBody('');
         setNotes((prevState) => {
-            return [newNote, ...prevState];
+            return [currentNote, ...prevState];
         });
-    }
+    };
 
-    function handleNoteClick(e) {
-        let note = notes.find(
-            (element) => element.noteID === e.target.dataset.noteid
-        );
+    const handleNoteClick = (e) => {
+        let note = notes.find((element) => element._id === e.target.dataset.id);
         setCurrentNote(note);
-        setTitle(note.noteTitle);
-        setBody(note.noteBody);
-        const newNotes = [
-            note,
-            ...notes.filter((item) => item.noteID !== note.noteID),
-        ];
+        setTitle(note.title);
+        setBody(note.body);
+        const newNotes = [note, ...notes.filter((item) => item._id !== note._id)];
         setNotes(newNotes);
-    }
+    };
 
-    function handleDeleteClick(e) {
+    const handleDeleteClick = (e) => {
         e.stopPropagation();
-        const noteId = e.target.previousSibling.dataset.noteid;
-        const newNotes = notes.filter((note) => note.noteID != noteId);
-        setNotes(newNotes);
-        if (newNotes.length > 0) {
-            setCurrentNote(newNotes[0]);
-            setTitle(newNotes[0].noteTitle);
-            setBody(newNotes[0].noteBody);
+        e.preventDefault();
+        const id = e.target.previousSibling.dataset.id;
+        console.log(id);
+        fetch(process.env.REACT_APP_API_URL + '/deletenote', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + accessToken,
+            },
+            credentials: 'include',
+            body: JSON.stringify({
+                'id': id,
+            }),
+        })
+            .then((response) => {
+                console.log(response);
+                if (response.ok) {
+                    setNotes(notes.filter((note) => note._id != id));
+                }
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        if (notes.length > 0) {
+            setCurrentNote(notes[0]);
+            setTitle(notes[0].noteTitle);
+            setBody(notes[0].noteBody);
         }
-    }
+    };
 
-    const signOut = () => {
-
-    }
+    const signOut = () => {};
 
     useEffect(() => {
         setCurrentNote((prevState) => {
             return {
                 ...prevState,
-                noteTitle: title,
-                noteBody: body,
+                title: title,
+                body: body,
             };
         });
     }, [title, body]);
 
     useEffect(() => {
+        debouncedSave(currentNote, accessToken);
+    }, [currentNote]);
+
+    useEffect(() => {
         setNotes((prevState) => {
             return prevState.map((element) =>
-                element.noteID === currentNote.noteID ? currentNote : element
+                element._id === currentNote._id ? currentNote : element
             );
         });
     }, [currentNote]);
 
-    useEffect(() => debouncedSave(notes), [notes]);
-
-
-    useEffect(() => {
-        console.log(currentUser);
-    }, [currentUser])
     return (
-        <main className="main">
-            <div className="left">
-                <p className="welcome-message">
-                    {" "}
-                    Hello <span> {currentUser}! </span>{" "}
+        <main className='main'>
+            <div className='left'>
+                <p className='welcome-message'>
+                    {' '}
+                    Hello <span> {currentUser}! </span>{' '}
                 </p>
-                <button onClick={createNewNote} className="new-note">
-                    {" "}
-                    <i className="fa-solid fa-plus"></i> New{" "}
+                <button onClick={createNewNote} className='new-note'>
+                    {' '}
+                    <i className='fa-solid fa-plus'></i> New{' '}
                 </button>
-                <div className="note-names">
-                    <p className="header"> Notes </p>
-                    <div className="names-scroll">
+                <div className='note-names'>
+                    <p className='header'> Notes </p>
+                    <div className='names-scroll'>
                         {notes &&
                             notes.map((e) => (
                                 <div
-                                    className="noteTitleDiv"
+                                    className='noteTitleDiv'
                                     onClick={handleNoteClick}
                                     key={nanoid()}
                                 >
                                     <input
-                                        className="title"
-                                        type="button"
-                                        data-noteid={e.noteID}
-                                        name={e.noteTitle}
+                                        className='title'
+                                        type='button'
+                                        data-id={e._id}
+                                        name={e.title}
                                         key={nanoid()}
-                                        value={e.noteTitle || "Untitled"}
+                                        value={e.title || 'Untitled'}
                                     ></input>
                                     <i
                                         onClick={handleDeleteClick}
-                                        className="fa-solid fa-trash"
+                                        className='fa-solid fa-trash'
                                     ></i>
                                 </div>
                             ))}
                     </div>
                 </div>
-                <p className="sign-out" onClick={signOut}>
-                    {" "}
-                    Sign Out{" "}
+                <p className='sign-out' onClick={signOut}>
+                    {' '}
+                    Sign Out{' '}
                 </p>
             </div>
-            <div className="right">
+            <div className='right'>
                 {notes && notes.length === 0 && (
-                    <div className="no-notes">
+                    <div className='no-notes'>
                         <p> You have no notes. </p>
                     </div>
                 )}
                 {notes && notes.length > 0 && Object.keys(notes).length > 0 && (
-                    <div className="note">
+                    <div className='note'>
                         <input
-                            name="noteTitle"
-                            className="note-title"
-                            type="text"
+                            name='noteTitle'
+                            className='note-title'
+                            type='text'
                             onChange={(e) => setTitle(e.target.value)}
-                            placeholder="Untitled"
-                            value={title || ""}
+                            placeholder='Untitled'
+                            value={title || ''}
                         />
                         <textarea
-                            name="noteBody"
-                            className="note-body"
-                            type="text"
+                            name='noteBody'
+                            className='note-body'
+                            type='text'
                             onChange={(e) => setBody(e.target.value)}
-                            placeholder="Enter your note here..."
-                            value={body || ""}
+                            placeholder='Enter your note here...'
+                            value={body || ''}
                         >
-                            {" "}
+                            {' '}
                         </textarea>
                     </div>
                 )}
